95ebd8ed6396dcbc0e8b2226914ec2b082fea1f4
uposledzony <chrustek123456789@gmail.com>
2020-01-25 18:29:03 +0100

    task 1.5.1 added static methods

--- src/test/java/edu/kis/vh/nursery/RhymersDemo.java
+++ src/test/java/edu/kis/vh/nursery/RhymersDemo.java
@@ -8,30 +8,50 @@ class RhymersDemo {
     private static final int HANOI_INDEX = 3;
     private static final int DATA_COUNT = 15;
     private static final int RHYMERS_COUNT = 3;
+    private static final int BOUND = 20;
+
 
     public static void main(String[] args) {
-        RhymersFactory factory = new DefaultRhymersFactory();
+        DefaultCountingOutRhymer[] rhymers = getDefaultCountingOutRhymers(new DefaultRhymersFactory());
 
-        DefaultCountingOutRhymer[] rhymers = {factory.getStandardRhymer(), factory.getFalseRhymer(),
-                factory.getFIFORhymer(), factory.getHanoiRhymer()};
+        countRhymers(rhymers);
 
+        countRhymerRandom(rhymers[HANOI_INDEX]);
+        //ctrl + alt + (left or right arrow) are used to navigate to the previous place (cursor) in project files (in Intellij)
+        printResults(rhymers);
+
+        printRejectedReport(rhymers[HANOI_INDEX]);
+
+    }
+
+    private static void countRhymerRandom(DefaultCountingOutRhymer rhymer) {
+        java.util.Random rn = new java.util.Random();
+        for (int i = 1; i < DATA_COUNT; i++)
+            rhymer.countIn(rn.nextInt(BOUND));
+    }
+
+    private static void countRhymers(DefaultCountingOutRhymer[] rhymers) {
         for (int i = 1; i < DATA_COUNT; i++)
             for (int j = 0; j < RHYMERS_COUNT; j++)
                 rhymers[j].countIn(i);
+    }
 
-        java.util.Random rn = new java.util.Random();
-        for (int i = 1; i < DATA_COUNT; i++)
-            rhymers[HANOI_INDEX].countIn(rn.nextInt(20));
-		//ctrl + alt + (left or right arrow) are used to navigate to the previous place (cursor) in project files (in Intellij)
+    private static void printRejectedReport(DefaultCountingOutRhymer rhymer) {
+        System.out.println("total rejected is "
+                + ((HanoiRhymer) rhymer).reportRejected());
+    }
+
+    private static void printResults(DefaultCountingOutRhymer[] rhymers) {
         for (int i = 0; i < rhymers.length; i++) {
             while (!rhymers[i].callCheck())
                 System.out.print(rhymers[i].countOut() + "  ");
             System.out.println();
         }
+    }
 
-        System.out.println("total rejected is "
-                + ((HanoiRhymer) rhymers[HANOI_INDEX]).reportRejected());
-
+    private static DefaultCountingOutRhymer[] getDefaultCountingOutRhymers(RhymersFactory factory) {
+        return new DefaultCountingOutRhymer[]{factory.getStandardRhymer(), factory.getFalseRhymer(),
+                    factory.getFIFORhymer(), factory.getHanoiRhymer()};
     }
 
 }
